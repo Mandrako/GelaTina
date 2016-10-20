ffa2bd2057ff7ffa47e9005bee08da4730145d64
Mandrako <marcus@olkerud.com>
2016-10-20 14:37:15 +0200

    Created a level generator
    I created an in-editor level generator component that takes Texture2D as
    input, maps the color of the pixels to prefabs and instantiates them in
    the scene.

---
+++ GelaTina/Assets/Editor/LevelGeneratorInspector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(LevelGenerator))]
+public class LevelGeneratorInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        LevelGenerator levelGenerator = (LevelGenerator) target;
+        if (GUILayout.Button("Generate Level"))
+        {
+            levelGenerator.LoadMap();
+        }
+
+        if (GUILayout.Button("Clear Level"))
+        {
+            levelGenerator.EmptyMap();
+        }
+    }
+}
+
+
---
+++ GelaTina/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LevelGenerator : MonoBehaviour
+{
+    public int TileSize = 16;
+    public Texture2D LevelMap;
+    public ColorToPrefab[] ColorsToPrefabs;
+
+    public void EmptyMap()
+    {
+        while (transform.childCount > 0)
+        {
+            Transform c = transform.GetChild(0);
+            c.SetParent(null);
+            DestroyImmediate(c.gameObject);
+        }
+    }
+
+    public void LoadMap()
+    {
+        EmptyMap();
+
+        Color32[] allPixels = LevelMap.GetPixels32();
+        int width = LevelMap.width;
+        int height = LevelMap.height;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                SpawnPrefabAt(allPixels[(y * width) + x], x * TileSize, y * TileSize);
+            }
+        }
+
+    }
+
+    void SpawnPrefabAt(Color32 color, int x, int y)
+    {
+        if (color.a <= 0)
+        {
+            return;;
+        }
+
+        foreach (ColorToPrefab ctp in ColorsToPrefabs)
+        {
+            if (ctp.color.Equals(color))
+            {
+                GameObject go = (GameObject) Instantiate(ctp.prefab, new Vector3(x, y, 0), Quaternion.identity);
+                go.transform.parent = this.transform;
+                return;
+            }
+        }
+
+        Debug.LogError("No prefab matches the color: " + color.ToString());
+    }
+}
+
+[Serializable]
+public class ColorToPrefab
+{
+    public Color32 color;
+    public GameObject prefab;
+}
