59ac92ac86353c80a1cdb3024c4ecec398d07265
Mandrako <marcus@olkerud.com>
2016-11-17 16:20:08 +0100

    Added main menu scene
    I created a scene for the main menu and created a very basic script to
    manage the buttons.

---
+++ GelaTina/Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartNewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ToggleOptions()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
