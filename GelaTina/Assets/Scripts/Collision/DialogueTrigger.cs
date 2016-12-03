cef2504d86989f8c8add84e61cd386ead0192896
Mandrako <marcus@olkerud.com>
2016-12-03 15:34:02 +0100

    Implemented the dialogue trigger

--- GelaTina/Assets/Scripts/Collision/DialogueTrigger.cs
+++ GelaTina/Assets/Scripts/Collision/DialogueTrigger.cs
@@ -1,26 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class DialogueTrigger : MonoBehaviour
 {
     public string targetTag = "Player";
+    public float displayTime = 3.0f;
+    public string[] texts;
+
+    private Canvas _targetCanvas;
+    private Text _targetTextField;
 
     void OnTriggerEnter2D(Collider2D target)
     {
+        var trigger = GetComponent<Collider2D>();
+        trigger.enabled = false;
+
+        _targetCanvas = target.GetComponentInChildren<Canvas>(true);
+        _targetTextField = target.GetComponentInChildren<Text>(true);
+
         if (target.gameObject.tag == targetTag)
         {
-            OnDialogue(target.gameObject);
-            OnDestroy();
+            if(_targetCanvas != null && _targetTextField != null)
+            {
+                StartCoroutine(OnDialogue(target.gameObject));
+            }
         }
     }
 
-    protected virtual void OnDialogue(GameObject target)
+    protected virtual IEnumerator OnDialogue(GameObject target)
     {
+        _targetCanvas.gameObject.SetActive(true);
+        int i = 0;
 
+        while(i < texts.Length)
+        {
+            _targetTextField.text = texts[i];
+            i++;
+            yield return new WaitForSeconds(displayTime);
+        }
+
+        OnDestroy();
     }
 
     protected virtual void OnDestroy()
     {
+        _targetCanvas.gameObject.SetActive(false);
         Destroy(gameObject);
     }
 }
