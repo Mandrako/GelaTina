5a8a55ca43545dbddad7231e82e8f21505be9b80
Mandrako <marcus@olkerud.com>
2017-01-24 16:30:14 +0100

    Added a fader script
    I added a fadermanager that can fade the screen to black and back. I
    intend to use this when transitioning between scenes and maybe player
    deaths.

---
+++ GelaTina/Assets/Scripts/Managers/FadeManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FadeManager : MonoBehaviour
+{
+    public float Step = 0.1f;
+
+    private static FadeManager _instance;
+    private float _transparence;
+
+	void Awake ()
+	{
+	    if (_instance == null)
+	    {
+	        _instance = this;
+            DontDestroyOnLoad(gameObject);
+	    }
+	    else
+	    {
+	        Destroy(gameObject);
+	    }
+	}
+
+    public static void StartFadeOut()
+    {
+        _instance.StartCoroutine("FadeOut");
+    }
+
+    public static void StartFadeIn()
+    {
+        _instance.StartCoroutine("FadeIn");
+    }
+
+    IEnumerator FadeOut()
+    {
+        while (_transparence < 1)
+        {
+            _transparence += Step * Time.deltaTime;
+            GetComponent<CanvasGroup>().alpha = _transparence;
+
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        while (_transparence > 0)
+        {
+            _transparence -= Step * Time.deltaTime;
+            GetComponent<CanvasGroup>().alpha = _transparence;
+
+            yield return null;
+        }
+    }
+}
---
+++ GelaTina/Assets/Scripts/Managers/PlayerSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject PlayerPrefab;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
