843622b9bea8cafbd803414a168658cb9370b594
Mandrako <marcus@olkerud.com>
2017-02-14 16:28:43 +0100

    Continued working on scene transitioning
    An endzone script was added that is supposed to trigger a scene
    transition.

---
+++ GelaTina/Assets/Scripts/Collision/EndZone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndZone : MonoBehaviour {
+    public string targetTag = "Player";
+    public int nextSceneIndex = -1;
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.tag == targetTag)
+        {
+            OnEnterZone(target.gameObject);
+        }
+    }
+
+    protected void OnEnterZone(GameObject target)
+    {
+        Scene nextScene = SceneManager.GetSceneByBuildIndex(nextSceneIndex);
+
+        Debug.Log("nextScene: " + nextScene.name + " - " + nextScene.buildIndex);
+
+        if (nextScene.buildIndex != -1)
+        {
+            SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            Debug.LogError("Can't find next scene!");
+        }
+    }
+}
