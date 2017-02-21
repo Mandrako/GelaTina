372a5db33f58387738a126fcb93d4238e1aa778e
Mandrako <marcus@olkerud.com>
2017-02-21 17:09:43 +0100

    Added a manager for the pause menu
    I created a manager script for the pause menu where I intend to
    implement menu functionality. The manager currently toggles the pause
    menu canvas when the escape button is pressed (I will need to make this
    more dynamic to enable controller support in the future).

---
+++ GelaTina/Assets/Scripts/UI/PauseMenuManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    private Canvas _pauseCanvas;
+
+	void Start ()
+	{
+	    _pauseCanvas = GetComponentInChildren<Canvas>();
+	    if (_pauseCanvas != null)
+	    {
+	        _pauseCanvas.gameObject.SetActive(false);
+	    }
+	    else
+	    {
+	        Debug.LogError("Couldn't find a canvas for the pause menu in children!");
+	    }
+	}
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _pauseCanvas.gameObject.SetActive(!_pauseCanvas.gameObject.activeSelf);
+        }
+    }
+
+    public void Save()
+    {
+        // TODO: implement a save function
+        throw new NotImplementedException();
+    }
+
+    public void Quit()
+    {
+        // TODO: implement a quit function
+        throw new NotImplementedException();
+    }
+}
