787a228809eaa352836445871c8cbe44935a3921
Mandrako <marcus@olkerud.com>
2017-03-14 00:07:33 +0100

    Created a door
    I created a door that opens when the player steps within the trigger
    radius and closes after the player leaves.

---
+++ GelaTina/Assets/Scripts/Door.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public float closeDelay = .5f;
+
+    Animator _animator;
+    BoxCollider2D _collider2D;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _collider2D = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    void EnableCollider2D()
+    {
+        _collider2D.enabled = true;
+    }
+
+    void DisableCollider2D()
+    {
+        _collider2D.enabled = false;
+    }
+
+    public void Open()
+    {
+        _animator.SetInteger("AnimState", 1);
+    }
+
+    public void Close()
+    {
+        StartCoroutine(CloseNow());
+    }
+
+    IEnumerator CloseNow()
+    {
+        yield return new WaitForSeconds(closeDelay);
+        _animator.SetInteger("AnimState", 2);
+    }
+}
