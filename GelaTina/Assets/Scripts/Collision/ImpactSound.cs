efa0f7b0d4b15ed56366f812e584eb2f0e579287
Mandrako <marcus@olkerud.com>
2017-03-15 22:53:48 +0100

    Added slime audio
    I created a script that plays a random sound on collision.

---
+++ GelaTina/Assets/Scripts/Collision/ImpactSound.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactSound : MonoBehaviour
+{
+    public AudioClip[] clips;
+
+    private AudioSource _source;
+
+    void Start()
+    {
+        _source = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Debug.Log("coll: " + coll.gameObject.name);
+
+        if(coll.gameObject.layer == LayerMask.NameToLayer("Solid") && clips.Length > 0)
+        {
+            Debug.Log("play");
+
+            var randomIndex = Random.Range(0, clips.Length);
+
+            _source.clip = clips[randomIndex];
+
+            _source.Play();
+        }
+    }
+}
