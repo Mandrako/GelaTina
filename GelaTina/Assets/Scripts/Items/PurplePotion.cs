0d3400e559ba9dea54ae83b17be65d33d2b6a651
Mandrako <marcus@olkerud.com>
2017-03-18 14:07:05 +0100

    Final commit before TopKek GameJam
    This is it.

--- GelaTina/Assets/Scripts/Items/PurplePotion.cs
+++ GelaTina/Assets/Scripts/Items/PurplePotion.cs
@@ -9,10 +9,22 @@ public class PurplePotion : Collectable {
     protected override void OnCollect(GameObject target)
     {
         var equipBehaviour = target.GetComponent<Equip>();
+        var duckBehaviour = target.GetComponent<Duck>();
+        var wallslideBehaviour = target.GetComponent<WallSlide>();
         
         if(equipBehaviour != null)
         {
             equipBehaviour.currentItem = itemId;
+        }
+
+        if(duckBehaviour != null)
+        {
+            duckBehaviour.enabled = true;
+        }
+
+        if(wallslideBehaviour != null)
+        {
+            wallslideBehaviour.enabled = false;
         }
 
         var circleCollider = target.gameObject.GetComponent<CircleCollider2D>();
